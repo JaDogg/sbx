851b3c77646b788d7294d1b267f403424b8e071a
JaDogg <JaDogg@users.noreply.github.com>
2020-07-21 21:01:46 +0100

    chore: Created setup.py
    Signed-off-by: JaDogg <JaDogg@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,37 @@
+from codecs import open
+from inspect import getsource
+from os.path import abspath, dirname, join
+
+from setuptools import setup, find_packages
+
+here = abspath(dirname(getsource(lambda: 0)))
+
+with open(join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="sbx",
+    version="0.2.1",
+    description="StudyBox (SBX) - Terminal Flashcards",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JaDogg/sbx",
+    author="Bhathiya Perera",
+    author_email="JaDogg@users.noreply.github.com",
+    python_requires=">=3.6",
+    license="MIT",
+    package_data={"": ["*.md"]},
+    install_requires=["prompt-toolkit>=3.0.5"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="study flashcard terminal",
+    packages=find_packages(exclude=["tests"]),
+    entry_points={"console_scripts": ["sbx = sbx.sbx_main:main"]},
+    setup_requires=["wheel"],
+)
