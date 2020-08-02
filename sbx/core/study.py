19a7f62324fd987abd1602bb451a5e426de6c443
JaDogg <JaDogg@users.noreply.github.com>
2020-08-02 16:51:15 +0100

    chore: Make CardStack.iter() call repeatable
    Signed-off-by: JaDogg <JaDogg@users.noreply.github.com>

--- sbx/core/study.py
+++ sbx/core/study.py
@@ -19,29 +19,34 @@ class CardStack:
         filter_to_last_zero: bool = False,
     ):
         """
-        Init a card stack with .md files in given location
+        Initialize a card stack with `.md` files in given location
 
-        * `path` - path containing sbx .md files
-        * `recursive` - scan recursively for .md files
+        * `path` - path containing SBX `.md` files
+        * `recursive` - scan recursively for `.md` files
         * `include_unscheduled` - include cards not scheduled for today
         * `filter_to_leech` - create a subset where all cards are leech
         * `filter_to_last_zero` - create a subset where all cards have
             marked last time as zero
         """
         self._path = Path(path)
-        if recursive:
-            self._files = self._path.glob("**/*.md")
-        else:
-            self._files = self._path.glob("*.md")
+        self._recursive = recursive
         self._all = include_unscheduled
         self._filter_to_leech = filter_to_leech
         self._filter_to_last_zero = filter_to_last_zero
+
+    def _get_files(self):
+        if self._recursive:
+            files = self._path.glob("**/*.md")
+        else:
+            files = self._path.glob("*.md")
+
+        return files
 
     def iter(self) -> Iterator[Card]:
         """
         Get cards we need to study (depend on how you constructed the class)
         """
-        for card_file in self._files:
+        for card_file in self._get_files():
             if not card_file.is_file():
                 continue
             try:
